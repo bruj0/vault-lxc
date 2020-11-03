fa11fc07ca729241f88be042572b953a41ba4c93
bruj0 <ramakandra@gmail.com>
2020-11-03 13:20:23 +0100

    updated to last version

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ lxc network set lxdbr0 ipv6.address none
 lxc network set lxdbr1 ipv6.address none
 lxc network set lxdbr2 ipv6.address none 
 
-systemd-resolve --interface lxdbr0 --set-dns $LXDBR0_IP --set-domain lxd
-systemd-resolve --interface lxdbr1 --set-dns $LXDBR1_IP --set-domain lxd
-systemd-resolve --interface lxdbr2 --set-dns $LXDBR2_IP --set-domain lxd
+sudo systemd-resolve --interface lxdbr0 --set-dns $LXDBR0_IP --set-domain lxd
+sudo systemd-resolve --interface lxdbr1 --set-dns $LXDBR1_IP --set-domain lxd
+sudo systemd-resolve --interface lxdbr2 --set-dns $LXDBR2_IP --set-domain lxd
 
 echo -e "auth-server=lxdbr0\nauth-zone=lxd\ndns-loop-detect\nserver=192.168.50.1\nno-resolv" | lxc network set lxdbr0 raw.dnsmasq -
 echo -e "auth-server=lxdbr1\nauth-zone=lxd\ndns-loop-detect\nserver=192.168.50.1\nno-resolv" | lxc network set lxdbr1 raw.dnsmasq -
